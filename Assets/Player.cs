b91998227532c200a4df795841250a2227aeea7e
Jeremiah <jeremiahteee@gmail.com>
2018-03-18 01:01:09 -0400

    ColorSwitch initial project

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float jumpForce = 10f;
+	public Rigidbody2D circle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0)){
+			circle.velocity = Vector2.up * jumpForce;
+		}
+	}
+}
---
+++ Assets/Rotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+	public float rotationSpeed = 100f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0f, 0f, rotationSpeed * Time.deltaTime);
+	}
+}
