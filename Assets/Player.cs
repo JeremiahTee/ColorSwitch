7eb7f757428ec30fcc67188b9c096f8a3c9cc4c6
Jeremiah <jeremiahteee@gmail.com>
2018-04-30 00:34:59 -0400

    obstacle[], Singleton player instance, reload upon falling out of border

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,10 +8,19 @@ public class Player : MonoBehaviour {
 	public string currentColor;
 	public float jumpForce = 10f;
 	public Rigidbody2D circle;
+	public GameObject[] obstacle;
 	public SpriteRenderer sr;
 	public Color blue, yellow, pink, purple;
 	public static int score = 0;
 	public Text scoreText;
+
+	//Singleton, class that will only ever have one instance during scene
+	public static Player instance;
+
+	void Awake(){
+		instance = this;
+	}
+
 	// Use this for initialization
 	void Start () {
 		setRandomColor ();
@@ -22,7 +31,9 @@ public class Player : MonoBehaviour {
 		if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0)){
 			circle.velocity = Vector2.up * jumpForce;
 		}
-
+		if (instance.transform.position.y < -100f) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex); //reload index
+		}
 		scoreText.text = score.ToString(); //convert int to string
 	}
 
@@ -31,6 +42,12 @@ public class Player : MonoBehaviour {
 			score++;
 			setRandomColor ();
 			Destroy (collision.gameObject);
+			int rand = Random.Range (0, 2);
+			if (rand == 0) {
+				Instantiate (obstacle[0], new Vector2(transform.position.x, transform.position.y + 7f), transform.rotation);
+			} else {
+				Instantiate (obstacle[1], new Vector2(transform.position.x, transform.position.y + 7f), transform.rotation);
+			}
 			return;
 		}
 
